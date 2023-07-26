4b67aa66a918524c4c35463e2695c2e840278b8f
Al Masum <almasum77@gmail.com>
2023-07-27 01:28:02 +0600

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'Reddi':'#F85559;'
+      },
+    },
+    plugins: [],
+  }
+
